69c7dbf74c9967d348a36738f6922aed5c2c423c
zuoji <501348474@qq.com>
2023-04-30 14:54:25 +0800

    修改deploy文件

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,6 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-git push -f https://github.com/zhaoshibao/gulimall-pro-learn-docs.git master:gh-pages
+git push -f git@github.com:zhaoshibao/gulimall-pro-learn-docs.git master:gh-pages
 
 cd -
